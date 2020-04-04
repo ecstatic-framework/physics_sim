921a8b9b04fc1fec06388e73f5d87c6f1065e0e9
Alexander Pilafian <sikanrong@gmail.com>
2020-04-04 21:04:26 +0100

    Fixing local mix deps, link directly to the ecstatic master branch (as this code depends on unpublished changes)

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule PhysicsSim.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecstatic, path: "../ecstatic"}
+      {:ecstatic, github: "ecstatic-framework/ecstatic"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
